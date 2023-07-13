4888c9453fef8973b31a7d3f32c784cb55cc1d5d
Toma Morris <btmorr@users.noreply.github.com>
2023-07-13 15:19:54 -0400

    Refactor code to separate server code from raft state

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,43 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Union
+from typing import List, Union
 from store import Store
-from raft import State, AppendEntries, AppendEntriesResponse
+from raft import Entry, State, Op
 
 
+# all FastAPI and Pydantic code should be limited to the server module
 class ReadResponse(BaseModel):
     value: Union[str, None]
+
+
+class LogEntry(BaseModel):
+    op: Op
+    key: str
+    value: Union[str, None]
+    term: int
+
+    def as_entry(self) -> Entry:
+        """
+        re-serialization to prevent leaking pydantic model into internal code,
+        may be technically unnecessary due to duck-typing, but this can be used
+        to satisfy the type-checker by doing something along the lines of
+        [le.as_entry() for le in log_entries]
+        """
+        return Entry(self.op, self.key, self.value, self.term)
+
+
+class AppendEntries(BaseModel):
+    term: int
+    leaderId: int
+    prevLogIndex: int
+    prevLogTerm: int
+    entries: List[LogEntry] = list()
+    leaderCommit: int
+
+
+class AppendEntriesResponse(BaseModel):
+    term: int
+    success: bool
 
 
 def build_app():
@@ -36,6 +67,14 @@ def build_app():
     # ----- Raft Router -----
     @app.post("/rpc/append")
     async def rpc_append_entries(req: AppendEntries) -> AppendEntriesResponse:
-        return state.append_entries_receiver(req)
+        """handles AppendEntry RPC requests for writes and heartbeats"""
+        res = state.append_entries(
+            term=req.term,
+            leader_id=req.leaderId,
+            prev_log_index=req.prevLogIndex,
+            prev_log_term=req.prevLogTerm,
+            entries=req.entries,  # noqa  -- see note in LogEntry.as_entry
+            leader_commit=req.leaderCommit)
+        return AppendEntriesResponse(term=res.term, success=res.success)
 
     return app
